627c272aea407bbc0ad2d0f06534a4f6d8f03878
Sami <sami.filjak@student.kdg.be>
2023-03-21 15:26:17 +0100

    d

--- src/be/kdg/view/mainmenu/MainMenuView.java
+++ src/be/kdg/view/mainmenu/MainMenuView.java
@@ -4,6 +4,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
@@ -15,12 +16,9 @@ import javafx.scene.text.Font;
 public class MainMenuView extends VBox {
     private Button start;
     private Button highScore;
-    private Label memoryGame;
-    private Label madeBy;
-    private Label sami;
-    private Label haron;
     private Image achtergrond;
     private Button exit;
+    private Image memory;
 
     public MainMenuView() {
         this.initialiseNodes();
@@ -30,12 +28,9 @@ public class MainMenuView extends VBox {
 
     private void initialiseNodes() {
         start = new Button("Start");
-        memoryGame = new Label();
-        madeBy = new Label();
-        sami = new Label();
-        haron = new Label();
         highScore = new Button("Highscore");
         exit = new Button("Exit");
+        memory = new Image("memorace_logo.png");
     }
 
     public Button getStart() {
@@ -51,25 +46,19 @@ public class MainMenuView extends VBox {
     }
 
     private void layoutNodes() {
-        this.achtergrond = new Image("/mainmenu.jpg", true);
+        this.achtergrond = new Image("/mainmenubackground.png", true);
         this.setBackground(new Background(new BackgroundImage(achtergrond, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+        ImageView imageView = new ImageView(memory);
+        imageView.setX(400);
+        imageView.setY(150);
+        imageView.setFitWidth(400);
+        imageView.setFitHeight(260);
         setSpacing(10);
         start.setPrefSize(160, 40);
-        memoryGame.setFont(Font.font("Verdana", 31));
-        memoryGame.setTextFill((Paint.valueOf("#ffffff")));
-        memoryGame.setText("MEMORACE");
-        madeBy.setFont(Font.font("Verdana", 15));
-        madeBy.setTextFill((Paint.valueOf("#ffffff")));
-        madeBy.setText("Made by:");
-        sami.setFont(Font.font("Verdana", 15));
-        sami.setTextFill((Paint.valueOf("#ffffff")));
-        sami.setText("Sami Filjak");
-        haron.setFont(Font.font("Verdana", 15));
-        haron.setTextFill((Paint.valueOf("#ffffff")));
-        haron.setText("Haron Badaoui");
         highScore.setPrefSize(160,40);
         exit.setPrefSize(160,40);
         setAlignment(Pos.CENTER);
-        getChildren().addAll(memoryGame, madeBy, sami, haron, start, highScore, exit);
+        getChildren().add(imageView);
+        getChildren().addAll(start, highScore, exit);
     }
 }
